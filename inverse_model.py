b16172eb929968f8656a64b1e06f59a9e15084ca
Benjamin Hills <benjaminhhills@gmail.com>
2020-05-18 22:50:15 -0600

    Add a function for weakly nonlinear problems.

--- inverse_model.py
+++ inverse_model.py
@@ -13,8 +13,11 @@ import time
 from forward_model import numerical_model
 from scipy.interpolate import interp1d
 
-z_data,T_data = np.load('data/input_data.npy')
+mult = np.matmul
+inv = np.linalg.inv
+tr = np.transpose
 
+z_data,T_data = np.load('data/input_data.npy')
 
 def norm2(m,z_data=z_data,T_data=T_data):
     T_interp = interp1d(m.z,m.T)
@@ -22,22 +25,78 @@ def norm2(m,z_data=z_data,T_data=T_data):
     norm = np.sum((T_data-T_mod)**2.)
     return norm
 
-
 def cost(modeled_Temp, T_param, t_param, measured_Temp=T_data, regularization=0.0):
+    """
+    """
     # Return the cost function given the measured temperature profile and our model output
     if regularization > 0:
         reg = np.nansum(np.gradient(np.gradient(T_param,t_param),t_param) ** 2.0)
     else:
         reg = 0.0
     unreg_cost = np.sqrt(np.nansum((modeled_Temp - measured_Temp) ** 2.0))
-    #plt.plot((modeled_Temp - measured_Temp) ** 2.0)
     return unreg_cost + regularization * reg, unreg_cost
 
+def weakly_nonlinear(f,norm,zdata,Tdata,C,m_init,mstep,Niter=10,
+                     solution_tolerance=1e-5,verbose=True):
+    """
 
-def monte_carlo_run(params,Tdata,zdata,
+    """
+
+    # Set up matrices for output
+    m_out = np.zeros((Niter,len(m_init)))
+    m_out[0,:] = m_init  # model at each iteration
+    d_out = np.zeros((Niter,len(Tdata)))
+    d_out[0,:]=f(m_init)     # modeled data vector at each iteration
+    if verbose:
+        print('Initial model')
+        print('Data residual norm:',norm(Tdata-f(m_init)))
+
+    # Initialize the Jacobian to be filled in
+    A = np.zeros((len(Tdata),len(m_init)))
+
+    # Start iterating
+    m = m_init.copy()
+    for i in range(1,Niter):
+        print('Iteration #', i)
+
+        # Calculate the data residual vector
+        dd = Tdata - f(m)
+        ddw = mult(C,dd)
+
+        # Calculate the Jacobian matrix.
+        f0 = f(m)
+        for j in range(len(m)):
+            mJ = m.copy()
+            mJ[j] += mstep[j]
+            A[:,j] = (f(mJ)-f0)/mstep[j];
+        Aw = mult(C,A)
+
+        # Linearize equation to solve for the model step, dm
+        dm = mult(inv(mult(tr(Aw),Aw)),mult(tr(Aw),ddw))
+
+        while norm(Tdata-f(m+dm)) > norm(dd) or (m[2]+dm[2])<0:
+            dm /= 2.
+
+        # Update the model
+        m += dm
+
+        # Print and store the iteration metrics
+        if verbose:
+            print('Data residual norm:',norm(Tdata-f(m)))
+            print('Norm of model step, dm:',norm(dm))
+        m_out[i,:] = m
+        d_out[i,:]=f(m)
+
+        # If convergence criteria is met, break out of loop
+        if i>1 and norm(dm)<solution_tolerance:
+            return m_out[:i+1,:],d_out[:i+1,:]
+
+def monte_carlo(params,Tdata,zdata,
                     tinit=-50000.,dt_var=5000.,
                     n_iterates=500,stepsize=0.2,regularization=0.):
-    ###Perform a suite of runs to get our distribution. We start with an initial guess, and move around.
+    """
+    Perform a suite of runs to get our distribution. We start with an initial guess, and move around.
+    """
 
     # this is just to check how long it took
     now = time.time()
@@ -83,7 +142,6 @@ def monte_carlo_run(params,Tdata,zdata,
             params = params_pert.copy()
             if i%100 == 0:
                 print('Successful Run:',i,np.random.normal(0., stepsize),stepsize,misfits[i + 1, :])
-
             # increment only upon acceptance of the guess
             i += 1
 
